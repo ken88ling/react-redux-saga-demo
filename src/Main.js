f115834ffd65750d8cd84f783b951a393b56f1ae
Ken Ling <ken88ling@gmail.com>
2019-05-12 11:44:01 +0800

    rename App to Main Entry

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+// I make it easy like oop programming entry
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default Main;
