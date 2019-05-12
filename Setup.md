8a88902c2fb1a9bdb9e157b6ec904ac402979093
Ken Ling <ken88ling@gmail.com>
2019-05-12 12:43:11 +0800

    3. store.dispatch - action

--- Setup.md
+++ Setup.md
@@ -3,3 +3,4 @@ Configuration of Redux , react-redux
 1. Create the store
 2. Create reducer (function) to the store
 3. Dispatch an action
+4. Update the store - reducer
--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ import * as serviceWorker from "./serviceWorker";
 
 import { createStore } from "redux";
 
-function reducer() {
+function reducer(state, action) {
+  if (action.type === "changeState") {
+    return action.payload.newState;
+  }
   return "state";
 }
 
 const store = createStore(reducer);
+//console.log(store.getState());
 
+const action = {
+  type: "changeState",
+  payload: {
+    newState: "New state"
+  }
+};
+
+//console.log(store);
+store.dispatch(action);
 console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
